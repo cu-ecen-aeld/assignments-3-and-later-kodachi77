974a0e762dd88a012efd0c78a184dfec7d0088fb
kodachi77 <12465849+kodachi77@users.noreply.github.com>
2024-08-17 10:04:19 -0400

    minor change

--- student-test/assignment1/Test_validate_username.c
+++ student-test/assignment1/Test_validate_username.c
@@ -18,7 +18,7 @@ void test_validate_my_username()
      * TODO: Replace the line below with your code here as described above to verify your /conf/username.txt 
      * config file and my_username() functions are setup properly
      */
-    char* buffer = malloc_username_from_conf_file();
-    TEST_ASSERT_TRUE_MESSAGE(my_username(), buffer);
-    free(buffer);
+    char* conf_username = malloc_username_from_conf_file();
+    TEST_ASSERT_TRUE_MESSAGE(my_username(), conf_username);
+    free(conf_username);
 }
