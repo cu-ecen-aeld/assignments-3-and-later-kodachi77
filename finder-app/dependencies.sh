688091844fbd1b2f4692bcf3bea94012769d5553
kodachi77 <12465849+kodachi77@users.noreply.github.com>
2024-08-25 16:57:31 -0400

    initial commit for part 2

--- finder-app/dependencies.sh
+++ finder-app/dependencies.sh
@@ -6,3 +6,4 @@
 sudo apt-get install -y libssl-dev
 sudo apt-get install -y u-boot-tools
 sudo apt-get install -y qemu
+sudo apt-get install qemu-system-arm qemu-system-aarch64
--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -12,69 +12,154 @@ BUSYBOX_VERSION=1_33_1
 FINDER_APP_DIR=$(realpath $(dirname $0))
 ARCH=arm64
 CROSS_COMPILE=aarch64-none-linux-gnu-
+FORCE_CLEAN=
+COMPILE_MODULES=
 
 if [ $# -lt 1 ]
 then
-	echo "Using default directory ${OUTDIR} for output"
+    echo "Using default directory ${OUTDIR} for output"
 else
-	OUTDIR=$1
-	echo "Using passed directory ${OUTDIR} for output"
+    OUTDIR=$1
+    echo "Using passed directory ${OUTDIR} for output"
 fi
 
 mkdir -p ${OUTDIR}
-
-cd "$OUTDIR"
-if [ ! -d "${OUTDIR}/linux-stable" ]; then
-    #Clone only if the repository does not exist.
-	echo "CLONING GIT LINUX STABLE VERSION ${KERNEL_VERSION} IN ${OUTDIR}"
-	git clone ${KERNEL_REPO} --depth 1 --single-branch --branch ${KERNEL_VERSION}
+if [ ! -d "${OUTDIR}" ]; then
+    echo "Output directory ${OUTDIR} cannot be created"
+    exit 1
 fi
-if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
+
+# Make OUTDIR path absolute
+OUTDIR=$(realpath ${OUTDIR})
+
+cd $OUTDIR
+if [ ! -d "${OUTDIR}/linux-stable" ]; then
+    # Clone only if the repository does not exist.
+    echo "CLONING GIT LINUX STABLE VERSION ${KERNEL_VERSION} IN ${OUTDIR}"
+    git clone ${KERNEL_REPO} --depth 1 --single-branch --branch ${KERNEL_VERSION}
+fi
+
+if [ ! -e "${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image" ]; then
     cd linux-stable
     echo "Checking out version ${KERNEL_VERSION}"
     git checkout ${KERNEL_VERSION}
 
-    # TODO: Add your kernel build steps here
+    echo "Compiling linux"
+
+    # Don't forget to install bison, flex and openssl. Use:
+    # sudo apt install flex bison libssl-dev
+
+    if [ "$FORCE_CLEAN" ]; then
+	make ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE mrproper
+    fi
+
+    if [ ! -e .config ]; then
+	make ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE defconfig
+    fi
+
+    make -j4 ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE all
+
+    if [ "$COMPILE_MODULES" ]; then
+        make ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE modules
+    fi
+
+    make ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE dtbs
 fi
 
 echo "Adding the Image in outdir"
+cp "${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image" "$OUTDIR/"
 
 echo "Creating the staging directory for the root filesystem"
-cd "$OUTDIR"
-if [ -d "${OUTDIR}/rootfs" ]
-then
-	echo "Deleting rootfs directory at ${OUTDIR}/rootfs and starting over"
-    sudo rm  -rf ${OUTDIR}/rootfs
+if [ -d "${OUTDIR}/rootfs" ]; then
+    echo "Deleting rootfs directory at ${OUTDIR}/rootfs and starting over"
+    sudo rm  -rf "${OUTDIR}/rootfs"
 fi
 
-# TODO: Create necessary base directories
+mkdir -p "${OUTDIR}/rootfs" && cd "${OUTDIR}/rootfs"
+mkdir -p bin dev etc home lib lib64 proc sbin sys tmp usr var
+mkdir -p usr/bin usr/lib usr/sbin
+mkdir -p var/log
 
-cd "$OUTDIR"
+echo "Compiling busybox"
+cd $OUTDIR
 if [ ! -d "${OUTDIR}/busybox" ]
 then
-git clone git://busybox.net/busybox.git
+    git clone git://busybox.net/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
-    # TODO:  Configure busybox
 else
     cd busybox
 fi
 
-# TODO: Make and install busybox
+if [ "$FORCE_CLEAN" ]; then
+    make distclean
+
+fi
+
+if [ ! -e .config ]; then
+    make defconfig
+fi
+
+make ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE
+make ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE CONFIG_PREFIX="${OUTDIR}/rootfs" install
 
 echo "Library dependencies"
-${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
-${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
+${CROSS_COMPILE}readelf -a ${OUTDIR}/rootfs/bin/busybox | grep "program interpreter"
+${CROSS_COMPILE}readelf -a ${OUTDIR}/rootfs/bin/busybox | grep "Shared library"
 
-# TODO: Add library dependencies to rootfs
+# Add library dependencies to rootfs
+SYSROOT_DIR=$(${CROSS_COMPILE}gcc -print-sysroot)
+SYSROOT_DIR=$(realpath $SYSROOT_DIR)
 
-# TODO: Make device nodes
+for file in $(${CROSS_COMPILE}readelf -a ${OUTDIR}/rootfs/bin/busybox | sed -n "s#.*program interpreter: \(.*\)]#${SYSROOT_DIR}\1#p"); do
+    # Copy each file to the destination directory
+    relative_path="${file#$SYSROOT_DIR}"
+    echo "Copying ${file} to ${OUTDIR}/rootfs/${relative_path}"
+    cp -L "$file" "${OUTDIR}/rootfs/${relative_path}"
+done
 
-# TODO: Clean and build the writer utility
+for file in $(${CROSS_COMPILE}readelf -a ${OUTDIR}/rootfs/bin/busybox | sed -n "s#.*Shared library: \[\(.*\)\]#${SYSROOT_DIR}/lib64/\1#p"); do
+    # Copy each file to the destination directory
+    relative_path="${file#$SYSROOT_DIR}"
+    echo "Copying ${file} to ${OUTDIR}/rootfs/${relative_path}"
+    cp -L "$file" "${OUTDIR}/rootfs/${relative_path}"
+done
 
-# TODO: Copy the finder related scripts and executables to the /home directory
-# on the target rootfs
+# Make device nodes
+cd "${OUTDIR}/rootfs"
+if  [ -e dev/null ]; then 
+    sudo mknod dev/null c 1 3
+    sudo chmod 666 dev/null
+fi
 
-# TODO: Chown the root directory
+if [ -e dev/console ]; then
+    sudo mknod dev/console c 5 1
+    sudo chmod 600 dev/console
+fi
 
-# TODO: Create initramfs.cpio.gz
+# Clean and build the writer utility
+cd "$FINDER_APP_DIR"
+if [ "$FORCE_CLEAN" ]; then
+    make CROSS_COMPILE=$CROSS_COMPILE clean
+fi
+make CROSS_COMPILE=$CROSS_COMPILE
+
+# Copy the finder related scripts and executables to the /home directory on the target rootfs
+cp writer finder.sh finder-test.sh autorun-qemu.sh "${OUTDIR}/rootfs/home/"
+
+conf_relative_path=$(realpath --relative-to=$(realpath .) $(readlink -f conf))
+cp -r $conf_relative_path "${OUTDIR}/rootfs/home/${conf_relative_path}"
+cp -a conf "${OUTDIR}/rootfs/home/"
+
+# Chown the root directory
+sudo chown -R root:root "${OUTDIR}/rootfs"
+
+# Create initramfs.cpio.gz
+cd "${OUTDIR}/rootfs"
+if [ -e "${OUTDIR}/initramfs.cpio.gz" ]; then
+    rm -f "${OUTDIR}/initramfs.cpio.gz"
+fi
+find . | cpio -H newc -ov --owner root:root > ${OUTDIR}/initramfs.cpio
+gzip ${OUTDIR}/initramfs.cpio
+
+echo "Build complete!!!"
